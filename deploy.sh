3b2f2ba0f2e928a8b65692ebd23cf85153a5fdbc
HaithamMahajna <haithammahajna98@gmail.com>
2025-06-25 22:36:28 +0300

    creating image and adding deploy.yaml file

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo docker build -t myapp .
-kind load docker-image myapp
+sudo kind load docker-image myapp
 sudo kubectl apply -f deploy.yaml
